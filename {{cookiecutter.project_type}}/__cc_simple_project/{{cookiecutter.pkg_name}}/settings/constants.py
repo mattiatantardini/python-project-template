1d533e0fa1a997240b9b5a8074bc2fc6af30e361
Mattia Tantardini <mattia.tantardini@gmail.com>
2022-10-12 16:34:18 +0200

    feat: added log settings, added docs on how to use files

---
+++ {{cookiecutter.project_type}}/__cc_simple_project/{{cookiecutter.pkg_name}}/settings/constants.py
@@ -0,0 +1,4 @@
+"""
+Define here all variables and quantities that are known and fixed once program parameters
+are known. That is, constants can be parametrized wrt to  particular program parameter.
+"""
